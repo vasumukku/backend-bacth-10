864a30ce98a869cf79f2f2fa4d043e71ddfc0750
vasumukku <mukkuvasu01@gmail.com>
2026-07-10 12:38:43 +0530

    added body allnote api data

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./body.css"
 import { Link } from 'react-router'
+import axios from "axios"
+
 
 const Body = () => {
+  const[data,setData]=useState([])
+    const allnotesfun=async() => {
+      try {
+        const allnotedata=await axios.get("http://localhost:5000/allnotes")
+        console.log(allnotedata)
+        setData(allnotedata.data)
+      } catch (e) {
+        console.log(e.message)
+      }
+    }
+
+  useEffect(()=>{
+    allnotesfun()
+  },[])
+
   return (
     <>
     <div className="logout-box">
@@ -16,6 +33,33 @@ const Body = () => {
 
     <div style={{textAlign:"center"}}>
       <h1>All Notebooks </h1>
+    </div>
+
+
+    <div className='card-container'>
+
+     {
+  data.map((element, index) => {
+    return (
+      <div className="card" key={index}>
+        <img
+          src={element.imageid}
+          alt="book img"
+          className="img-style-book"
+        />
+
+        <h4 style={{color:"red",fontSize:"20px"}}>{element.name}</h4>
+        <h3 > Author :<span style={{color:"green"}}>{element.author}</span></h3>
+        <div className='price-row-style'>
+           <p> <b>price :</b>{element.price}</p>
+           <button className='add-to-cart-style'>Add to cart</button>
+        </div>
+       
+      </div>
+    );
+  })
+}
+        
     </div>
     </>
   )
